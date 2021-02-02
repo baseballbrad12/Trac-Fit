bb10ad9df682e71e256f208abe51ee4cf5989265
Bradley Gosiengfiao <baseballbrad12@gmail.com>
2021-02-01 17:52:26 -0800

    Creating localhost in server.js and creating controllers folder.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const controller = require('.controllers');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(controller);
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/workout", {
